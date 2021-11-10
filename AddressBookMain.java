74378ab389e8971da78548da49bd0990dd35c530
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-10 19:19:44 +0530

    Added: code for AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,5 +4,16 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
 	System.out.println("Welcome to AddressBook Program");
+
+        PersonDetails person = new PersonDetails();
+        person.Name("Rahul");
+        person.LastName("Labade");
+        person.Address("Shivaji Nagar");
+        person.City("Nagpur");
+        person.State("MP");
+        person.Zip(465123);
+        person.PhoneNumber("789456133");
+        person.Email("rahulgha05@gmail.com");
+        System.out.println(person.toString());
     }
 }
